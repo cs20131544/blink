25b4718d8846b2a953082c48bb1613009028e23e
cs20131544 <37132972+cs20131544@users.noreply.github.com>
2018-03-08 17:02:31 +0900

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -1,1 +1,12 @@
+Blink blink(D4);
 
+void setup()
+{
+  Serial.begin();
+  Serial.println("\nBlink Version 1.0 Your_First_Name Your_Last_Name");
+}
+void loop()
+{
+  blink.on(500);
+  blonk.off(300);
+}
