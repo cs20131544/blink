bfa75377ec044283c03bc2b1cc4f8829f2c033f5
cs20131544 <37132972+cs20131544@users.noreply.github.com>
2018-03-08 16:59:53 +0900

    Update blink.cpp

--- blink.cpp
+++ blink.cpp
@@ -1,1 +1,17 @@
+#include "blink.h"
 
+Blink::Blink(int pin)
+{
+  pinMode(pin, OUTPUT);
+  _pin = pin;
+}
+void Blink::on()
+{
+  digitalWrite(_pin,HIGH);
+  delay(500);
+}
+void Blink::off()
+{
+  digitalWrite(_pin,LOW);
+  dealy(300);
+}
